859436e936611b52f35b9f76dee763491bddbf66
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-09 15:08:48 +0200

    add collatz-conjecture

---
+++ collatz-conjecture/lib/collatz_conjecture.dart
@@ -0,0 +1,11 @@
+class CollatzConjecture {
+  int steps(int n, {int step = 0}) {
+    if (n <= 0)
+      throw ArgumentError('Only positive numbers are allowed',
+          'an ArgumentError with the message "Only positive numbers are allowed"');
+    if (n == 1) return step;
+    return n % 2 == 0
+        ? steps(n / 2 as int, step: step + 1)
+        : steps(3 * n + 1, step: step + 1);
+  }
+}
