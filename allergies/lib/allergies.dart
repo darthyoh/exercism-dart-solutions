e561fea8f4bed919f3c5e80fb3b6aba8f79250cc
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-10 13:51:47 +0200

    add allergies

---
+++ allergies/lib/allergies.dart
@@ -0,0 +1,26 @@
+class Allergies {
+  static const allergies = [
+    "eggs",
+    "peanuts",
+    "shellfish",
+    "strawberries",
+    "tomatoes",
+    "chocolate",
+    "pollen",
+    "cats",
+  ];
+
+  bool allergicTo(String element, int score) => allergies
+      .asMap()
+      .entries
+      .where((entry) => score >> entry.key % 2 != 0)
+      .map((entry) => entry.value)
+      .contains(element);
+
+  List<String> list(int num) => allergies
+      .asMap()
+      .entries
+      .where((entry) => num >> entry.key % 2 != 0)
+      .map((entry) => entry.value)
+      .toList();
+}
