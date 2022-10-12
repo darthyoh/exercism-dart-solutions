4bc1391c1b2c4357d94acf91c8545449d51c7c83
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 12:43:39 +0200

    correct pangram

--- pangram/lib/pangram.dart
+++ pangram/lib/pangram.dart
@@ -1,11 +1,19 @@
 class Pangram {
   bool isPangram(String str) {
-    if (str == '') return false;
-    List<String> mapping = [];
+    var map = Map<String, int>.fromIterables(
+        "abcdefghijklmnopqrstuvwxyz".split(""),
+        List<int>.generate(26, (index) => 0));
 
-    for (String c in str.toLowerCase().replaceAll(" ", "").split("")) {
-      if (mapping.contains(c)) return false;
-      mapping.add(c);
+    str
+        .toLowerCase()
+        .replaceAll(RegExp(r"[^a-z]"), "")
+        .split("")
+        .forEach((element) {
+      map[element] = map[element]! + 1;
+    });
+
+    for (int v in map.values) {
+      if (v == 0) return false;
     }
     return true;
   }
