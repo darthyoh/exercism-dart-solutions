265955e49f09cf0db1a473d83c395c70edb7d436
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 15:03:26 +0200

    add matching-brackets

--- matching-brackets/lib/matching_brackets.dart
+++ matching-brackets/lib/matching_brackets.dart
@@ -1,3 +1,23 @@
 class MatchingBrackets {
-  bool isPaired(String str) {}
+  bool isPaired(String str) {
+    str = str.replaceAll(RegExp(r'[^\(\{\[\]\}\)]'), "");
+
+    List<String> current = [];
+    for (String c in str.split("")) {
+      if (current.length == 0 && "])}".contains(c)) return false;
+      if ("({[".contains(c)) {
+        current.add(c);
+      }
+      if ((c == ")" && current[current.length - 1] == "(") ||
+          (c == "}" && current[current.length - 1] == "{") ||
+          (c == "]" && current[current.length - 1] == "[")) {
+        current.removeLast();
+      } else if ((c == ")" && current[current.length - 1] != "(") ||
+          (c == "}" && current[current.length - 1] != "{") ||
+          (c == "]" && current[current.length - 1] != "[")) {
+        return false;
+      }
+    }
+    return current.length == 0;
+  }
 }
