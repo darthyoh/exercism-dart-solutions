2b3dfa332871e0df7434d7dd5b06661964bf1f2b
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 12:23:38 +0200

    correct nth-prime

--- nth-prime/lib/nth_prime.dart
+++ nth-prime/lib/nth_prime.dart
@@ -1,17 +1,21 @@
 class NthPrime {
   int prime(int n) {
+    if (n == 0)
+      throw ArgumentError('There is no zeroth prime',
+          'an ArgumentError with the message "There is no zeroth prime"');
+    bool isPrime(int n) {
+      if (n < 2) return false;
+      for (int i = 2; i < n; i++) if (n % i == 0) return false;
+      return true;
+    }
+
     var candidate = 2;
     var found = 0;
     while (found != n) {
-      if (List.generate(candidate - 2, (e) => n % (e + 2) == 0)
-              .where((e) => e == false)
-              .length ==
-          0) {
-        found++;
-        break;
-      }
+      if (isPrime(candidate)) found++;
+      if (found == n) return candidate;
       candidate++;
     }
-    return candidate;
+    return 0;
   }
 }
