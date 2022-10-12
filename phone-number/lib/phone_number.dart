b4cc0179864c580f481cca30a9296fa2422b1a5d
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 12:51:49 +0200

    correct phone number

--- phone-number/lib/phone_number.dart
+++ phone-number/lib/phone_number.dart
@@ -1,14 +1,15 @@
 class PhoneNumber {
   String clean(String str) {
+    if (RegExp(r'[a-zA-Z]').hasMatch(str))
+      throw FormatException('letters not permitted');
     str = str.replaceAll(RegExp(r'[\+\.\-\(\) ]'), "");
     if (RegExp(r'[^0-9]').hasMatch(str))
       throw FormatException('punctuations not permitted');
-    if (RegExp(r'[a-zA-Z]').hasMatch(str))
-      throw FormatException('letters not permitted');
+
     if (str.length == 11 && str[0] != "1")
       throw FormatException('11 digits must start with 1');
-    if (str.length < 10 || str.length > 11)
-      throw FormatException('incorrect number of digits');
+    if (str.length > 11) throw FormatException('more than 11 digits');
+    if (str.length < 10) throw FormatException('incorrect number of digits');
     var re = RegExp(r'^\d*([0-9])\d{2}([0-9])\d{6}$');
     var match = re.firstMatch(str);
     if (match != null) {
@@ -21,6 +22,6 @@ class PhoneNumber {
       if (match.group(2) == "1")
         throw FormatException('exchange code cannot start with one');
     }
-    return str;
+    return str.length == 10 ? str : str.substring(1);
   }
 }
