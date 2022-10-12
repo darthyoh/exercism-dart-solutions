b4a9bb150353cc9bfdee264202e62f2af56d82e9
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 13:00:48 +0200

    correct prime-factors

--- prime-factors/lib/prime_factors.dart
+++ prime-factors/lib/prime_factors.dart
@@ -2,11 +2,11 @@ class PrimeFactors {
   List<int> factors(int n) {
     List<int> primes = [];
     if (n == 1) return primes;
-    while (primes.fold(0, (int a, int c) => a * c) != n) {
+    while (n != 1) {
       for (int i = 2; i <= n; i++) {
         if (n % i == 0) {
           primes.add(i);
-          n = n / i as int;
+          n = n ~/ i;
           break;
         }
       }
