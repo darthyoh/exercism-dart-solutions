834e7506223dd5911586047ef713a76be84d3206
Yohan Dufaud <ydufaud@sm-adn.fr>
2022-10-12 14:29:11 +0200

    correct word-count

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,12 +1,15 @@
 class WordCount {
-  Map<String,int> countWords(String sentence) => sentence
-    .toLowerCase()
-    .replaceAll(RegExp(r"[^a-z0-9']")," ")
-    .split(" ")
-    .where((w) => w!="")
-    .fold(<String,int>{}, (a,c) {
-      if(a[c]==null) a[c]=0;
-      a[c]=a[c]!+1;
-      return a;
-    });
+  Map<String, int> countWords(String sentence) => sentence
+          .toLowerCase()
+          .replaceAll(RegExp(r" \'"), " ")
+          .replaceAll(RegExp(r"\' "), " ")
+          .replaceAll(RegExp(r"\'$"), "")
+          .replaceAll(RegExp(r"[^a-z0-9\']"), " ")
+          .split(" ")
+          .where((w) => w != "")
+          .fold(<String, int>{}, (a, c) {
+        if (a[c] == null) a[c] = 0;
+        a[c] = a[c]! + 1;
+        return a;
+      });
 }
